3c6b2cf9cec40edea5f0241103419fa19ec223af
Akshara Ashokan <117848666+Akshara-Ashokan@users.noreply.github.com>
2022-12-11 12:11:05 +0530

    Create prgm30.py

---
+++ prgm30.py
@@ -0,0 +1,6 @@
+rows=int(input("enter number of rows:"))
+for i in range(1,rows+1):
+        for j in range(1,i+1):
+                square=i+j
+                print(i*j,end=' ')
+        print("\n")
