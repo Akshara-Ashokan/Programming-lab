0c09d9490837fc2a66f15369729a5468a2d9307e
Akshara Ashokan <117848666+Akshara-Ashokan@users.noreply.github.com>
2022-12-11 12:31:43 +0530

    Update prgm6.py

--- prgm6.py
+++ prgm6.py
@@ -1,3 +1,4 @@
+"""Write a Python program to get a string which is n (non-negative integer) copies of agiven string."""
 a=input("enter a string:")
 n=int(input("how many time do you want to repeat?"))
 print(a*n)
