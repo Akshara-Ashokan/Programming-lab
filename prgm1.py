29e6291d9a8a8a8dc7e22c43bcf00528bc9741eb
Akshara Ashokan <117848666+Akshara-Ashokan@users.noreply.github.com>
2022-12-11 12:16:00 +0530

    Update prgm1.py

--- prgm1.py
+++ prgm1.py
@@ -1,4 +1,5 @@
-fname=input("enter your first name:")
+"""Write a program that prompts the user to enter his first name and last name and then displays a message “Greetings!!! First name Last name”."""
+fame=input("enter your first name:")
 lname=input("enter your second name")
 name=fname+""+lname
 print("hello",name)
